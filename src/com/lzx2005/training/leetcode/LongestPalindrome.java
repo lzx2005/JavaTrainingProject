76dff47d321c17fd5a7d99ec6846b2c54e36f48d
john <crow2005@vip.qq.com>
2017-04-06 13:31:09 +0800

    LeetCode

--- src/com/lzx2005/training/leetcode/LongestPalindrome.java
+++ src/com/lzx2005/training/leetcode/LongestPalindrome.java
@@ -12,16 +12,11 @@ public class LongestPalindrome {
         char[] chars = s.toCharArray();
 
         for(int i=0;i<s.length();i++){
-            for(int j=s.length()-1;j>=i;j--){
-                if(j-i<maxLen){
-                    break;
-                }
+            for(int j=s.length()-1;j>=i&&j>=maxLen+i;j--){
                 if(chars[i]==chars[j]&&isPalindromic(chars,i,j)){
-                    if((j-i)>=maxLen){
-                        maxLen = j-i;
-                        max = new String(chars,i,maxLen+1);
-                        break;
-                    }
+                    maxLen = j-i;
+                    max = s.substring(i,j+1);
+                    break;
                 }
             }
         }
@@ -29,7 +24,6 @@ public class LongestPalindrome {
     }
 
     public boolean isPalindromic(char[] chars,int start,int end){
-
         while(start<end){
             if(chars[start]!=chars[end]){
                 return false;
@@ -50,11 +44,39 @@ public class LongestPalindrome {
 
         Long start = new Date().getTime();
         String s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-        String s1 = new LongestPalindrome().longestPalindrome(s);
+
+        String s1 = new Long2().longestPalindrome(s);
         System.out.println(s1);
 
         Long end = new Date().getTime();
         System.out.println((end-start)+"ms");
     }
 
+}
+
+class Long2{
+    private int lo, maxLen;
+
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2)
+            return s;
+
+        for (int i = 0; i < len-1; i++) {
+            extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
+            extendPalindrome(s, i, i+1); //assume even length.
+        }
+        return s.substring(lo, lo + maxLen);
+    }
+
+    private void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (maxLen < k - j - 1) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }
 }
